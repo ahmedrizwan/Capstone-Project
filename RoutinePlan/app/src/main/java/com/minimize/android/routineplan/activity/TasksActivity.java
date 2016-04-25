7d9800f61fa1ca42d657293201d7eca146e9c69c
Ahmed Rizwan <ahmedrizwan@outlook.com>
2016-04-25 14:36:20 +0400

    Retrieve Tasks

--- RoutinePlan/app/src/main/java/com/minimize/android/routineplan/activity/TasksActivity.java
+++ RoutinePlan/app/src/main/java/com/minimize/android/routineplan/activity/TasksActivity.java
@@ -2,12 +2,23 @@ package com.minimize.android.routineplan.activity;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.text.InputType;
 import android.view.View;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.minimize.android.routineplan.R;
 import com.minimize.android.routineplan.databinding.ActivityTasksBinding;
+import com.minimize.android.routineplan.databinding.ItemRoutineBinding;
+import com.minimize.android.rxrecycleradapter.RxDataSource;
+import com.minimize.android.rxrecycleradapter.SimpleViewHolder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import rx.functions.Action1;
 
 /**
  * Created by ahmedrizwan on 25/04/2016.
@@ -18,9 +29,42 @@ public class TasksActivity extends BaseActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     mBinding = DataBindingUtil.setContentView(this, R.layout.activity_tasks);
+    mBinding.recyclerViewRoutines.setLayoutManager(new LinearLayoutManager(this));
 
     final String routine = getIntent().getStringExtra("Routine");
     final Firebase tasksRef = new Firebase("https://routineplan.firebaseio.com/routines/" + android_id + "/" + routine);
+    final RxDataSource<String> rxDataSource = new RxDataSource<>(Collections.<String>emptyList());
+    rxDataSource.<ItemRoutineBinding>bindRecyclerView(mBinding.recyclerViewRoutines, R.layout.item_routine).subscribe(
+        new Action1<SimpleViewHolder<String, ItemRoutineBinding>>() {
+          @Override public void call(final SimpleViewHolder<String, ItemRoutineBinding> viewHolder) {
+            final ItemRoutineBinding viewDataBinding = viewHolder.getViewDataBinding();
+            final String item = viewHolder.getItem();
+            viewDataBinding.textView.setText(item);
+            viewDataBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+              @Override public void onClick(View v) {
+
+              }
+            });
+          }
+        });
+
+    tasksRef.addValueEventListener(new ValueEventListener() {
+      @Override public void onDataChange(DataSnapshot dataSnapshot) {
+        try {
+          List<String> routines = new ArrayList<String>();
+          for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            routines.add(snapshot.getKey());
+          }
+          rxDataSource.updateDataSet(routines).updateAdapter();
+        } catch (NullPointerException e) {
+
+        }
+      }
+
+      @Override public void onCancelled(FirebaseError firebaseError) {
+
+      }
+    });
 
     mBinding.fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
